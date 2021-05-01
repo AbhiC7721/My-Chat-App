c52432c3be4e9fdc8d5a2fd980ccfca233a24ebd
AbhiC7721 <abhic7721@gmail.com>
2021-05-01 20:03:36 +0530

    events chal 1

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,16 @@
+//CLIENT
+
 const socket = io()
 
-socket.on('countUpdated', (count) => {
-    console.log('The cou been updated!', count)
-})
+// socket.on('countUpdated', (count) => {
+//     console.log('The cou been updated!', count)
+// })
 
-document.querySelector('#increment').addEventListener('click', ()=> {
-    console.log('Clicked')
-    socket.emit('increment')
+// document.querySelector('#increment').addEventListener('click', ()=> {
+//     console.log('Clicked')
+//     socket.emit('increment')
+// })
+
+socket.on('message', (mes) => {
+    console.log(mes)
 })
