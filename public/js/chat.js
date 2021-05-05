751dd7e7a71d476e5a6f702c1c8c50d2f8a13797
AbhiC7721 <abhic7721@gmail.com>
2021-05-06 01:29:16 +0530

    Styling added part-1

--- public/js/chat.js
+++ public/js/chat.js
@@ -24,7 +24,7 @@ socket.on('message', (message) => {
 })
 
 socket.on('locationMessage', (locationMessage) => {
-    console.log(url)
+    // console.log(locationMessage.url)
     const html = Mustache.render(locationMessageTemplate, {
         url: locationMessage.url, 
         createdAt: moment(locationMessage.createdAt).format('h:mm a')
